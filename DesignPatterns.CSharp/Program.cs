c03ac2e473014b6e2f6bf4f1aa3c31c8c21df320
stualan <charleszhn@hotmail.com>
2021-03-14 23:43:59 +0800

    抽象工厂 懒汉式🐷 饿汉式😥

--- DesignPatterns.CSharp/Program.cs
+++ DesignPatterns.CSharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SingletonPattern;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DesignPatterns.CSharp
 {
@@ -6,7 +9,33 @@ namespace DesignPatterns.CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Singleton");
+            Enumerable.Range(1, 5).Select(
+                i => Task.Run(() =>
+            {
+                Console.WriteLine($"{Singleton.GetInstance().GetHashCode()}");
+            })
+                ).ToList();
+            //Enumerable.Range(1, 100).AsParallel().ForAll(i => { Console.WriteLine(i); });
+            Console.WriteLine($"Singleton1");
+            Enumerable.Range(1, 5).Select(i => Task.Run(() =>
+            {
+                Console.WriteLine($"{Singleton1.GetInstance().GetHashCode()}");
+            })).ToList();
+
+            Console.WriteLine($"Singleton2");
+            Enumerable.Range(1, 5).Select(i => Task.Run(() =>
+            {
+                Console.WriteLine($"{Singleton2.GetInstance().GetHashCode()}");
+            })).ToList();
+
+            Console.WriteLine($"Singleton3");
+            var ss=  Enumerable.Range(1, 5).Select(i => Task.Run(() =>
+            {
+                Console.WriteLine($"{Singleton3.GetInstance().GetHashCode()}");
+            })).ToList();
+
+            Console.ReadLine();
         }
     }
 }
