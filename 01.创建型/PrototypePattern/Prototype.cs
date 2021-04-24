91a1adf679bc8557a29a4884d5dee7a393e419c6
stualan <charleszhn@hotmail.com>
2021-04-24 22:09:07 +0800

    原型模式

---
+++ 01.创建型/PrototypePattern/Prototype.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern
+{
+    internal abstract class Prototype
+    {
+        public string Id { get; }
+
+        protected Prototype(string id) => Id = id;
+
+        public abstract Prototype Clone();
+    }
+
+    internal class ConcretePrototypeChild : Prototype
+    {
+        public ConcretePrototypeChild(string id) : base(id)
+        {
+        }
+
+        public override Prototype Clone()
+        {
+            return (Prototype)MemberwiseClone();
+        }
+    }
+}
---
+++ 01.创建型/PrototypePattern/SimpleResume.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern
+{
+    internal class SimpleResume : ICloneable
+    {
+        private string _name;
+        private string _email;
+
+        private string _timePeriod;
+        private string _company;
+
+        public void SetPersonalInfo(string name, string email)
+        {
+            _name = name;
+            _email = email;
+        }
+
+        public void SetWorkExperience(string company, string timePeriod)
+        {
+            _company = company;
+            _timePeriod = timePeriod;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{_name} {_email}");
+            Console.WriteLine($"工作经历：{_timePeriod} {_company}");
+        }
+
+        public object Clone() => MemberwiseClone();
+    }
+}
